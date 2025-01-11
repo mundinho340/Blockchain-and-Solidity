e9cb10150df76617ff277a14874db581c196edd5
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-11 14:48:20 -0300

    Update StackClub2_theory.sol

--- course/Arrrays/StackClub2_theory.sol
+++ course/Arrrays/StackClub2_theory.sol
@@ -1,3 +1,19 @@
 //Pop
 
 //Storage arrays also have access the pop member variable:
+
+import "forge-std/console.sol";
+contract Contract {
+	uint[] public numbers;
+
+    constructor() {
+		numbers.push(3);
+		numbers.push(4);
+		console.log(numbers.length); // 2
+		numbers.pop();
+		console.log(numbers.length); // 1
+        console.log(numbers[0]); // 3
+    }
+}
+
+//As you can see, pop will take the top element off the storage array.

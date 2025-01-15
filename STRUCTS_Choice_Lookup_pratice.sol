8d5a71aeafba7d5e225fba66e34d18fb16dc35e7
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-15 16:08:30 -0300

    Create STRUCTS_Choice_Lookup_pratice.sol

---
+++ STRUCTS_Choice_Lookup_pratice.sol
@@ -0,0 +1,6 @@
+//Your Goal: Find Vote
+
+    //Create an external, view function hasVoted which takes an address and returns a 
+    //bool indicating if the address has cast a vote or not.
+  //  Create an external, view function findChoice which takes an address and returns a Choices value
+  //indicating the choice on the vote cast by the address. For this function there is no need to worry about the case where a vote was not cast.

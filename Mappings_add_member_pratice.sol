79df27e386d8d13e1dd9cf8c598ff58e374348e0
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-17 12:28:54 -0300

    Create Mappings_add_member_pratice.sol

---
+++ Mappings_add_member_pratice.sol
@@ -0,0 +1,6 @@
+// Your Goal: Members Mapping
+
+//Create a public mapping called members which maps an address to a bool.
+//The bool will indicate whether or not the address is currently a member!
+//Create an external function called addMember which takes an address and adds it as a member.
+//You can do this by storing true in the data location corresponding to the address in the members mapping.

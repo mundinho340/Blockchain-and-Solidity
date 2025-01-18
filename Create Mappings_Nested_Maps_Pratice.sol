051cfbea3249d600d3f6b34b4958f90852b6cd4e
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-18 12:06:31 -0300

    Create Create Mappings_Nested_Maps_Pratice.sol

---
+++ Create Mappings_Nested_Maps_Pratice.sol
@@ -0,0 +1,4 @@
+// Your Goal: Make Connections
+
+   // Create a public mapping called connections which will map an address to a mapping of an address to a ConnectionTypes enum value.
+   // In theconnectWith function, create a connection from the msg.sender to the other address.

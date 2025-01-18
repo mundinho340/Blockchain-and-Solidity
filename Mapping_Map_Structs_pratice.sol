50f72d41e71cf88fb5b6328c02600cea10af9d09
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-18 10:03:53 -0300

    Create Mapping_Map_Structs_pratice.sol

---
+++ Mapping_Map_Structs_pratice.sol
@@ -0,0 +1,11 @@
+//Your Goal: User Mapping Tokens
+
+//Let's create a new token where every new user will receive 100 tokens!
+
+   // Create a public mapping called users that will map an address to a User struct.
+   // Create an external function called createUser. This function should create a new user and associate it to the msg.sender address in the users mapping.
+  //  The balance of the new user should be set to 100 and the isActive boolean should be set to true.
+
+//Contract Security
+
+//Ensure that the createUser function is called with an address that is not associated with an active user. Otherwise, revert the call.

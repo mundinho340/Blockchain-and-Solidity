b529e783f19eb40adff750579afa15d194de7fd2
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-18 10:04:23 -0300

    Update Mapping_Map_Structs_pratice.sol

--- Mapping_Map_Structs_pratice.sol
+++ Mapping_Map_Structs_pratice.sol
@@ -9,3 +9,30 @@
 //Contract Security
 
 //Ensure that the createUser function is called with an address that is not associated with an active user. Otherwise, revert the call.
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	struct User {
+		uint balance;
+		bool isActive;
+	}
+
+	mapping(address => User) public users;
+
+	function createUser() external {
+		if(users[msg.sender].isActive ){
+			revert("Bro ja estas activo");
+		}
+		users[msg.sender].balance= 100;
+		users[msg.sender].isActive= true;
+		
+	
+	}
+
+
+}
+
+
+

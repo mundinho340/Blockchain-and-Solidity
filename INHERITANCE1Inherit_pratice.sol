1ed8ff63483ba71fd815cf073e66f5d8c267c396
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-20 09:18:58 -0300

    Create INHERITANCE1Inherit_pratice.sol

---
+++ INHERITANCE1Inherit_pratice.sol
@@ -0,0 +1,20 @@
+//Your Goal: Make SuperHeroes
+
+//You'll notice that the SuperHeroes.sol file imports the Hero.sol read-only file. Your goal is to create two new contracts that inherit from the base Hero contract.
+
+  //  Create two new contracts Mage and Warrior which inherit both the function and the state variable from the Hero contract.
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+
+contract Mage is Hero{
+
+}
+
+contract Warrior is Hero{
+    
+}

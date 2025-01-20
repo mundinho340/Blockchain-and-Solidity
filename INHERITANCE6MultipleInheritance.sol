3685296a91a2f44014fb14ab0a51022d238a0952
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2025-01-20 15:32:22 -0300

    Create INHERITANCE6MultipleInheritance.sol

---
+++ INHERITANCE6MultipleInheritance.sol
@@ -0,0 +1,9 @@
+// Your Goal: Collectible Transferable
+
+//The Collectible contract now also inherits from Transferable, a contract which has not been created yet!
+
+    //Your goal is to create a new contract Transferable that will allow the Collectible to transfer its ownership to another address.
+   // In the Transferable contract, create a function called transfer which takes an address for the new owner.
+   // Have this function transfer ownership from the current owner to the new owner passed in.
+   // Ensure that this function can only be called by the current owner.
+   
